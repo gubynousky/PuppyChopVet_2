3bb72228a9fd4a7ef1362e41b4ee3f36bb604e94
gubynousky <ru.martinezs@duocuc.cl>
2025-11-12 02:47:42 -0300

    Se agrega un veterinario a Veterianrio.kt

--- app/src/main/java/cl/martinez/puppychopvet/model/Veterinario.kt
+++ app/src/main/java/cl/martinez/puppychopvet/model/Veterinario.kt
@@ -8,6 +8,7 @@ enum class Veterinario(val displayName: String, val especialidad: String) {
     DRA_LOPEZ("Dra. Ana López", "Cirugía"),
     DR_GONZALEZ("Dr. Pedro González", "Pediatría Veterinaria"),
     DRA_SILVA("Dra. María Silva", "Dermatología"),
+    DRA_SALAS("Dra. Estrella Salas", "Estética Canina"),
     DR_ROJAS("Dr. Juan Rojas", "Emergencias");
 
     companion object {
